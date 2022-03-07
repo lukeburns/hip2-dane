20e0607123f9944aab353b69c2cb709153610b72
Luke Burns <lukemburns@gmail.com>
2022-03-07 09:46:17 -0500

    add validation

--- hip2-dane.js
+++ hip2-dane.js
@@ -3,13 +3,26 @@ const { DANEAgent, setServers, lookup, dns } = require('https-dane')
 
 const agent = new DANEAgent()
 
-function fetchAddress (name, token = 'HNS') {
+const defaultOpts = {
+  token: 'HNS',
+  maxLength: 43,
+  validate: key => !!key && key.slice(0,3) === 'hs1' && key.length === 42
+}
+
+function fetchAddress (name, opts=defaultOpts) {
+  const { token, maxLength, validate } = opts || {}
   return new Promise((resolve, reject) => {
     const url = `https://${name}/.well-known/wallets/${token.toUpperCase()}`
     const req = https.get(url, { agent, lookup }, res => {
       let data = ''
       res.setEncoding('utf8')
-      res.on('data', chunk => data += chunk)
+      res.on('data', chunk => {
+        if (data.length + chunk.length > maxLength) {
+          req.destroy()
+          return reject('response too large')
+        }
+        data += chunk
+      })
       res.on('end', () => {
         if (res.statusCode >= 400) {
           const error = new Error(res.statusMessage)
@@ -17,7 +30,12 @@ function fetchAddress (name, token = 'HNS') {
 
           return reject(error)
         }
-        resolve(data.trim())
+        data = data.trim()
+        if (validate && validate(data)) {
+          resolve(data)
+        } else {
+          reject('invalid address')
+        }
       })
     })
     req.on('error', error => reject(error))
--- test.js
+++ test.js
@@ -2,13 +2,37 @@ const { test } = require('brittle')
 const { fetchAddress, setServers } = require('./hip2-dane.js')
 
 setServers([
-  '127.0.0.1:5350'
+  '127.0.0.1:9892'
 ])
 
 test('fetch address', async assert => {
   const expected = `hs1qshuyulxra3pqpwr40303t8pn79232zztuk4qgz`
-  const actual = await fetchAddress('iamfernando', 'hns')
+  const actual = await fetchAddress('iamfernando')
   assert.is(expected, actual)
+})
+
+test('invalid address', async assert => {
+  try {
+    await fetchAddress('invalid-addr.bb-8')
+  } catch (e) {
+    assert.pass(e.toString())
+  }
+})
+
+test('invalid type', async assert => {
+  try {
+    await fetchAddress('invalid-type.bb-8')
+  } catch (e) {
+    assert.pass(e.toString())
+  }
+})
+
+test('response too large', async assert => {
+  try {
+    await fetchAddress('large-size.bb-8')
+  } catch (e) {
+    assert.pass(e.toString())
+  }
 })
 
 // todo: failure test
